863a5d1f61cfa3d7c7d265528963b76ba5cfe691
sam.craig <sam.craig@vaynermedia.com>
2018-11-09 15:57:44 -0500

    removed unnecessary files

--- app.js
+++ app.js
@@ -3,22 +3,18 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const favicon = require('serve-favicon');
-const pug = require('pug');
-
+const port = 3000;
 const app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, 'public', 'views'));
+app.set('view engine', 'pug');
 app.use(favicon(path.join(__dirname, 'public', 'dist', 'images', 'favicon.small.png')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
-app.use('*.js', function(req, res, next) {
+app.use('*.js', (req, res, next) => {
     req.url = req.url + '.gz';
     res.set('Content-Encoding', 'gzip');
     res.set('Content-Type', 'text/javascript');
@@ -27,9 +23,9 @@ app.use('*.js', function(req, res, next) {
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-var nodemailer = require('nodemailer');
+const nodemailer = require('nodemailer');
 
-var transporter = nodemailer.createTransport({
+const transporter = nodemailer.createTransport({
     service: 'Gmail',
     port: 465,
     secure: true,
@@ -45,11 +41,11 @@ var transporter = nodemailer.createTransport({
 app.post('/contact', function (req, res) {
 
     // json response from contact form submission
-    var data = req.body;
+    const data = req.body;
 
     // Prevent key tampering from messing with email handler
-    if (!(data.name === undefined || data.email === undefined || data.message === undefined)) {
-        var mailOptions = {
+    if (!(data.name === 'undefined' || data.email === 'undefined' || data.message === 'undefined')) {
+        const mailOptions = {
             to: 'samc2198@gmail.com', // receiver address
             subject: 'Portfolio Inquiry', // subject line
             text: '[' + data.email + '] : ' +data.message // plain text body
@@ -81,8 +77,6 @@ app.use('/1zqzi0czkodjvp9lpu5f8gc5w8faw9.html', function(req, res, next) {
 
 // when a user requests /*, send them to the index
 // SPA view routing handled by angular-route
-app.use('/', function (req, res) {
-    res.render('index');
-});
+app.use('/', (req, res) => res.render('index'));
 
-module.exports = app;
+app.listen(port, () => console.log(`portfolio running on port: ${port}`))
